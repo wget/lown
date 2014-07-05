5a2b6bb0bc060e743487af3068d617812152fd85
William Gathoye <william@gathoye.be>
2014-07-05 13:53:40 +0200

    Have a more intelligent clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
+while [ 1 ]; do
+    read -p "[?] This script will remove all non .tex, .wiki or .pdf files. Do you want to continue? [y/N] " answer
+    case $answer in
+        [Yy]*)
 
-rm *.aux *.log *.pdf *.out
+            # Need to enbale extglob as the remove command we use needs this feature.
+            shopt extglob
+            extglobStatus=$(shopt extglob)
+            extglobStatus=${extglobStatus##extglob }
+            if [ "$extglobStatus" == "off" ]; then
+                echo "[+] extglob (Extended Pattern Matching) is disabled in Bash, enabling it..."
+                shopt -s extglob
+            fi
+            shopt extglob
+
+            echo "[+] Removing..."
+            rm !(*.tex|*.wiki|*.pdf)
+
+            break;;
+        [Nn]*|"")
+            break;;
+        *)
+            echo "[${RED}-${OFF}] Please answer by y or n.";;
+    esac
+done
+echo "[+] Exiting..."
